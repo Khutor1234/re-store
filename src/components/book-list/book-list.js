e554aa5af3e9241c511a16325bedb3d9dc544c1f
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-22 14:24:42 +0300

    added style and asynchronous data

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,53 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
-import BookListItem from '../book-list-item';
-import {withBookstoreService} from '../hoc';
-import {booksLoaded} from '../../actions';
-import {compose} from '../../utils';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
+import Spinner from '../spinner';
 
 import './book-list.css';
 
-class BookList extends Component{
+class BookList extends Component {
 
-    componentDidMount(){
-        //1. получить данные 
-        const {bookstoreService} = this.props;
-        const data = bookstoreService.getBooks();
-        console.log(data)
+  componentDidMount() {
+    // 1. receive data
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks()
+      .then((data) => booksLoaded(data))
 
-        //2.передать действие в store
-        this.props.booksLoaded(data)
+    // 2. dispacth action to store
+    
+  }
+
+  render() {
+    const { books, loading } = this.props;
+
+    if(loading){
+      return <Spinner/>
     }
 
-    render() {
-        const {books} = this.props;
-
-        return(
-            <ul>
-                {
-                    books.map((book) => {
-                        return(
-                            <li key = {book.id}><BookListItem book = {book} /></li>
-                        )
-                    })
-                }
-            </ul>
-        )
-    }
+    return (
+      <ul className="book-list">
+        {
+          books.map((book) => {
+            return (
+              <li key={book.id}><BookListItem book={book}/></li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return{
-        books: state.books
-    }
-}
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
+};
 
-const mapDispatchToProps =  {
-    booksLoaded
-}
+const mapDispatchToProps = {
+  booksLoaded
+};
 
 export default compose(
-    withBookstoreService(),
-    connect(mapStateToProps, mapDispatchToProps)
-    )(BookList);
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
